6027ae35d921ce60cfc66dc506d454d4fa47277d
Tico S. Thepsourinthone <ticotheps@gmail.com>
2019-03-27 14:04:39 -0400

    Updates the 001-roles.js file

--- seeds/001-roles.js
+++ seeds/001-roles.js
@@ -1,14 +1,14 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name')
+  return knex('roles')
     .truncate() // resets the primary key, in addition to cleaning the table
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
+      return knex('roles').insert([
+        { name: 'TA'},
+        { name: 'Student'},
+        { name: 'Staff'},
       ]);
     });
 };
